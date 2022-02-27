6b5e3f92463ad8b6ba3c29f6c7fa488835d61df5
mohmmed23 <mhmmedben3@gmail.com>
2022-02-27 17:22:57 +0200

    Create HTML and CSS files

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ let changeImgL = document.querySelector('.st-img-light')
 let changeImgD = document.querySelector('.st-img-dark')
 let logoBtn = document.querySelector('.logo-btn')
 let logoBtnD = document.querySelector('.logo-btn-d')
+let secImgD = document.querySelector('.sec-img-dark')
+let secImgL = document.querySelector('.sec-img-light')
+let x = document.querySelector('.sec')
 
 darkBtn.addEventListener('click', changeToDark)
 function changeToDark() {
@@ -13,4 +16,16 @@ function changeToDark() {
   changeImgD.classList.toggle('change-img')
   logoBtn.classList.toggle('change-img')
   logoBtnD.classList.toggle('change-img')
+  secImgL.classList.toggle('change-img')
+  secImgD.classList.toggle('change-img')
+
+  changeSection()
+}
+function changeSection() {
+  if (x.style.justifyContent === 'flex-end') {
+    x.style.justifyContent = 'flex-start'
+  } else {
+    x.style.justifyContent = 'flex-end'
+    // x.style.justifyContent === 'flex-end'
+  }
 }
